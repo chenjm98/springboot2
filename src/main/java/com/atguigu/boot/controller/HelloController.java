dbce815331bbde64e1e3dd251369cb444ee824f1
chenjm <ccww_2016@163.com>
2022-07-31 02:09:16 +0800

    first commit

---
+++ src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.atguigu.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ProjectName: springboot2
+ * @Package: com.atguigu.boot.controller
+ * @ClassName: HelloController
+ * @Author: chenjm
+ * @Description:
+ * @Date: 2022/7/26 23:47
+ */
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/aaaa")
+    public String hello(){
+        return "success";
+    }
+}
